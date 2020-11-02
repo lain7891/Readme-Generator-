cec00ad092e0f7fa17d737524c99c0d6b44c9e4d
Estefania <nialupo87@gmail.com>
2020-11-01 21:14:03 -0500

    created a testREADME.md file, trying out ways to display the questions and answers

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const fs = require("fs");
 const inquirer = require("inquirer");
-const generateMarkdown = require("./generateMarkdown")
+const generateMarkdown = require("./generateMarkdown");
+const util = require('util');
 
+const writeFileAsync = util.promisify(fs.writeFile);
 
 // array of questions for user
 const questions = [
-  inquirer
-    .prompt([
+ inquirer.prompt([
+
       {
         type: "input",
         message: "What is the title of your readme?",
@@ -37,17 +39,61 @@ const questions = [
         message: "How do you test this project?",
         name: "test",
       },
+      {
+        type: "list",
+        message: "Which license do you want to use?",
+        name: "license",
+        choices: ["MIT", "Mozilla Public License", "GPL", "Apache License"]
+      },
     ])
     .then((response) => {
       console.log(response);
-    }),
-];
+    //   const generateMarkdown = generateMarkdown(response);
+    }), 
+]
 
+
+    
+// fs.writeFile('testREADME.md', generateMarkdown(data), function (err) {
+//     if (err) throw err;
+//     console.log("Done");
+// });
+
+// questions()
+//   .then((data) => writeFileAsync('index.html', generateMarkdown(data)))
+//   .then(() => console.log('Successfully wrote to index.html'))
+//   .catch((err) => console.error(err));
 // function to write README file
 function writeToFile(fileName, data) {}
 
 // function to initialize program
-function init() {}
+// function init() {
+    // inquirer.prompt(questions).then(function(data){
+    //     console.log(data);
+    //     fs.writeFile("testREADME.md", generateMarkdown(data), function (err){
+    //         if (err) throw err 
+    //             console.log("Done!");
+    //         }
+    //     );
+    // });
+// }
+
+const init = async () => {
+    console.log("test");
+    try {
+        const answers = await questions();
+
+        const mdFile = generateMarkdown(answers);
+
+        await writeFileAsync('./testREADME.md', mdFile);
+
+        console.log("Success");
+    } catch (err) {
+        console.log(err);
+    }
+
+    };
 
 // function call to initialize program
 init();
+
